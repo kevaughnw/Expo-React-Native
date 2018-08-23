5b762eebf92e031b8d30fc8d0a28945dc8a4df73
hackerrithm <kemargalloway@gmail.com>
2018-08-23 15:22:52 -0500

    fix error {} for component usage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import Location from "./components/vehicleTallyScreen/location/location.containe
 import React, {PureComponent} from "react";
 export default class App extends PureComponent {
   render() {
-    return <Location />;
+    return <Location title="" />;
   }
 }
