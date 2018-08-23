23bef94030b5f45b8cbc9b8a7469aeae4a522084
kevaughn_williams@live.com <kevaughn_williams@live.com>
2018-08-23 12:49:56 -0500

    Modify folder structure

--- MotorVehicleManagerSystem/build/App.js
+++
@@ -1,8 +0,0 @@
-import Location from "./components/location/location.container";
-import React from "react";
-export default class App extends React.Component {
-    render() {
-        return React.createElement(Location, null);
-    }
-}
-//# sourceMappingURL=App.js.map
